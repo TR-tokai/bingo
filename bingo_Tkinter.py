de69256487bf5b7bc3fe5cbc773180a03a314185
TR-tokai <inakuro166@gmail.com>
2024-11-23 00:12:51 +0900

    Tkinterでビンゴ司会システムの作成開始
    ウィンドウ表示だけ実装

---
+++ bingo_Tkinter.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import random
+
+# ウィンドウ設定
+root = tk.Tk()
+root.title('Bingo')
+root.geometry('500x500')
+root.resizable(True, True)
+button_font_size = 50
+label_font_size = 60
+
+
+root.mainloop()
